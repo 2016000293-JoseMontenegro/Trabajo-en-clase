82dbaebba810881bee39cec9eecae4f49044e7f1
2016000293-JoseMontenegro <j.montenegro5adb@gmail.com>
2026-03-17 18:44:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Números Positivos y Negativos");
+        //Variables
+        int num;
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("Soy un número POSITIVO");
+        else
+            Console.WriteLine("Soy un número NEGATIVO");
+    }
+}
